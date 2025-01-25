fb14b6b767448360d824aabe62186c49df83933b
Crawford L <110504159+DefNotCrawf@users.noreply.github.com>
2025-01-25 01:00:48 +0000

    commit everything as I forgot to

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,20 @@ if ("serviceWorker" in navigator) {
 	});
 }
 
+// Function to enable dark mode
+function enableDarkMode() {
+	document.body.classList.add("dark-mode");
+	localStorage.setItem("darkMode", "enabled");
+	console.log("Dark mode enabled");
+}
+
+// Function to disable dark mode
+function disableDarkMode() {
+	document.body.classList.remove("dark-mode");
+	localStorage.setItem("darkMode", "disabled");
+	console.log("Dark mode disabled");
+}
+
 document.addEventListener("DOMContentLoaded", function () {
 	const darkModeSwitch = document.getElementById("darkModeSwitch");
 	if (darkModeSwitch) {
@@ -18,37 +32,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
 		darkModeSwitch.addEventListener("change", function (event) {
 			if (event.target.checked) {
-				document.body.classList.add("dark-mode");
-				localStorage.setItem("darkMode", "enabled");
+				enableDarkMode();
 			} else {
-				document.body.classList.remove("dark-mode");
-				localStorage.setItem("darkMode", "disabled");
+				disableDarkMode();
 			}
 		});
 	}
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('#registerForm');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        const csrfToken = document.querySelector('input[name="csrf_token"]').value;
-        const formData = new FormData(form);
-        formData.append('csrf_token', csrfToken);
+	const form = document.querySelector('#registerForm');
+	form.addEventListener('submit', function (event) {
+		event.preventDefault();
+		const csrfToken = document.querySelector('input[name="csrf_token"]').value;
+		const formData = new FormData(form);
+		formData.append('csrf_token', csrfToken);
 
-        fetch('/register', {
-            method: 'POST',
-            body: formData,
-            headers: {
-                'X-CSRFToken': csrfToken
-            }
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    });
+		fetch('/register', {
+			method: 'POST',
+			body: formData,
+			headers: {
+				'X-CSRFToken': csrfToken
+			}
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+			})
+			.catch(error => {
+				console.error('Error:', error);
+			});
+	});
 });
